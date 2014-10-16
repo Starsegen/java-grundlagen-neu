7dba6275240e4a2b36ec387421322ac188fd0e90
Starsegen <starsegen1987@hotmail.de>
2014-10-16 09:50:53 +0200

    objekt klasse

---
+++ Java/EigeneAusnahmeklasse/src/eigeneausnahmeklasse/FileNotFoundException.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eigeneausnahmeklasse;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Oliver Stabenow <starsegen1987@hotmail.de>
+ */
+public class FileNotFoundException extends IOException 
+{
+
+    public FileNotFoundException(String message) 
+    {
+        super(message);
+    }
+    
+    
+}
---
+++ Java/EigeneAusnahmeklasse/src/eigeneausnahmeklasse/NoDataFoundException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eigeneausnahmeklasse;
+
+/**
+ *
+ * @author Oliver Stabenow <starsegen1987@hotmail.de>
+ */
+public class NoDataFoundException extends Exception 
+{
+
+    public NoDataFoundException(String message) 
+    {
+        super(message);
+    }
+    
+    
+    
+}
